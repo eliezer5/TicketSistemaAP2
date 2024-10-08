f4107aeafdd9f777cb106d41901c0f60820990ac
Eliezer Terrero Rojas <eliezer_terrero@ucne.edu.do>
2024-10-08 16:04:45 -0400

    Creando sistemaDto

---
+++ app/src/main/java/com/ucne/edu/ticketsistemaretrofit/data/remote/dto/SistemasDto.kt
@@ -0,0 +1,7 @@
+package com.ucne.edu.ticketsistemaretrofit.data.remote.dto
+
+data class SistemasDto(
+    val sistemaId : Int?,
+    val nombreSistema: String?,
+    val descripcionSistema: String?
+)
