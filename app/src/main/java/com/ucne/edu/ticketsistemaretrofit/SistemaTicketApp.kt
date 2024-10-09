35d1b78c0e63efc65e467c2df0725d3d07ebce35
Eliezer Terrero Rojas <eliezer_terrero@ucne.edu.do>
2024-10-09 00:57:40 -0400

    Agregando SistemaTicketApp

---
+++ app/src/main/java/com/ucne/edu/ticketsistemaretrofit/SistemaTicketApp.kt
@@ -0,0 +1,8 @@
+package com.ucne.edu.ticketsistemaretrofit
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SistemaTicketApp: Application() {
+}
