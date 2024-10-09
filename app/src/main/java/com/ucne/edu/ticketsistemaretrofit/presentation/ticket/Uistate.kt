ed766651fe3257ee38d8a637cf1cb1aba8feb6b2
Eliezer Terrero Rojas <eliezer_terrero@ucne.edu.do>
2024-10-09 01:00:00 -0400

    Creando la Uistate de Ticket

---
+++ app/src/main/java/com/ucne/edu/ticketsistemaretrofit/presentation/ticket/Uistate.kt
@@ -0,0 +1,25 @@
+package com.ucne.edu.ticketsistemaretrofit.presentation.ticket
+
+import com.ucne.edu.ticketsistemaretrofit.data.remote.dto.TicketDto
+
+data class Uistate(
+    val ticketId: Int? = null,
+    val fecha: String = "",
+    val asunto: String = "",
+    val descripcion: String = "",
+    val tickets: List<TicketDto> = emptyList(),
+    val error: String = "",
+    val mensaje: String = "",
+    val sistemaId: Int? = null,
+    val solicitadoPor: String = ""
+)
+
+fun Uistate.toEntity() = TicketDto(
+    ticketId = ticketId,
+    fecha = fecha,
+    asunto = asunto,
+    descripcion = descripcion,
+    sistemaId = sistemaId,
+    solicitadoPor = solicitadoPor
+
+)
